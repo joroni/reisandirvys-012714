62e1de5dd5431721883d7bea6d7bc2356533761c
joroni <jraymund.niconi@gmail.com>
2014-01-27 16:01:45 +0800

    content

--- www/base.js
+++ www/base.js
@@ -91,8 +91,8 @@ function loginout() {
 }
 
 function listArticles() {
-	url = baseurl + '/phonegap/display_view';
-	//url = baseurl + '/phonegap/frontpage';
+	//url = baseurl + '/phonegap/display_view';
+	url = baseurl + '/drupalgap/views_datasource/drupalgap_content';
 	$username = '';
 	$("#latestlist").html("Retrieving Data...");
 	$.post(url, {
@@ -156,7 +156,8 @@ function sendStory() {
 	localStorage.phonegapStory = story;
 	uname = localStorage.phonegapName;
 	hash = localStorage.hash;
-	url = baseurl + '/phonegap/post';
+	//url = baseurl + '/phonegap/post';
+	url = baseurl + '/drupalgap/views_datasource/drupalgap_content';
 	$("#sentmessage").html('<img id="sending" title="sending" alt="sending" src="images/loadingt.gif" /><br /> Your Message is being sent. If this message does not change after two minutes, please check your network connectivity.')
 	$.post(url, {
 		username : uname,
